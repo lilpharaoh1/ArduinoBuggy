3a1fb28454d4c83cb0fe98d6226a2a0a7ddb6b6d
lilpharaoh1 <moustafe@tcd.ie>
2022-04-09 22:32:36 +0100

    init-commit

---
+++ networking/networking.ino
@@ -0,0 +1,52 @@
+#include <WiFiNINA.h>
+#include <Arduino_LSM6DS3.h>
+
+float x, y, z;
+float a, b, c;
+
+float pose = 90;
+
+//Networking
+char ssid[] = "GROUPZ777";
+char pass[] = "GROUPZ777";
+WiFiServer server(907);
+IPAddress ip;
+int wifi_counter = 0;
+
+
+void setup() {
+  
+if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+delay(1000);
+
+WiFi.beginAP(ssid, pass);
+ip = WiFi.localIP();
+server.begin();
+
+}
+
+void loop() {
+
+  if (wifi_counter >= 200) {
+    WiFiClient client = server.available();
+    if (client.connected()) { 
+      int data = client.read();
+      Serial.println(data); // Check what input is
+      if (data != null) {
+        client.println(pose); // send pose back
+      }
+    }
+  }
+  else {
+    wifi_counter++;
+  }
+  
+  IMU.readGyroscope(x, y, z);
+  input_x = x;
+  
+  pose += input_x
+}
